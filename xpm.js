c230768099bef732e9cc91ca584edcdc6403d9f4
Florian Margaine <florian@margaine.com>
2012-03-19 22:09:17 +0100

    First working version.

--- xpm.js
+++ xpm.js
@@ -1,35 +1,42 @@
 var XPM = {
     /**
-     * Only method of this scope: render
+     * Only method of this scope for now: create()
      * Takes multiple parameters... here is the list:
-     *   - width: width of the image
-     *   - height: height of the image
-     *   - nbColors: number of colors used
-     *   - nbCharactersPerColor: number of characters per color
+     *   - "<width> <height> <number of colors> <characters used per color>"
      *   - "<character used> c <color>" <- for each color used
      *   - Drawing of the XPM.
      *
-     * @see here for the inspiration: http://en.wikipedia.org/wiki/X_PixMap
+     * @see here for the inspiration: http://fr.wikipedia.org/wiki/X_PixMap
      */
-    render: function() {
-        var width = arguments[0]
-        var height = arguments[1]
-        var nbColors = arguments[2]
-        var nbCharactersPerColor = arguments[3]
-        var canvas = document.createElement('canvas')
+    create: function() {
+        var firstArgument = arguments[0].split(' '),
+            width = firstArgument[0],
+            height = firstArgument[1],
+            nbColors = firstArgument[2],
+            nbCharactersPerColor = firstArgument[3],
+            imgCharacters = [].slice.call(arguments, parseInt(nbColors) + 1),
+            canvas = document.createElement('canvas'),
+            ctx = canvas.getContext('2d'),
+            map = [],
+            characters,
+            color
         canvas.width = width
         canvas.height = height
-        var ctx = canvas.getContext('2d')
-        var map = []
         for (var i = 0; i < nbColors; ++i) {
-            var character = arguments[4 + i].substring(0, nbCharactersPerColor)
-            var color = arguments[4 + i].substring(nbCharactersPerColor + 3)
-            map.push({
-                character: character,
-                color: color
-            })
+            debugger
+            characters = arguments[1 + i].substring(0, parseInt(nbCharactersPerColor))
+            color = arguments[1 + i].substring(parseInt(nbCharactersPerColor) + 3)
+            map.push({characters: characters, color: color})
         }
-        for (var i = 0, l = map.length; i < l; ++i) {
+        for (var i = 0; i < height; ++i) {
+            for (var j = 0; j < width; ++j) {
+                for (var k = 0, l = map.length; k < l; ++k) {
+                    if (map[k].characters === imgCharacters[i][j]) {
+                        ctx.fillStyle = map[k].color
+                        ctx.fillRect(j, i, 1, 1)
+                    }
+                }
+            }
         }
         return canvas
     }
